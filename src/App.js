92684b808bceb10abdb33b842e0c72cd8cbec093
Avi Dangol <dangolavi9999@gmail.com>
2020-12-20 00:53:57 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+import data from './data.json';
+import Products from './Components/Products'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       products:data.products,
+       size:'',
+       sort:'',
+    }
+  }
+  
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href = '/'>React shopping Cart</a>
+        </header>
+        <main>
+          <div className = 'content'>
+            <div className = 'main'>
+              <Products products = {this.state.products}/>
+            </div>
+            <div className = 'sidebar'>
+              Cart Items
+            </div>
+          </div>
+        </main>
+        <footer>
+          All rights reserved
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
